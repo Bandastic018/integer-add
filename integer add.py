c2aff986983829e13692764e39a48788c9b2a59d
Ibrahim <ibrahimmuhammad018@gmail.com>
2022-06-13 14:03:07 +0000

    first commit

---
+++ integer add.py
@@ -0,0 +1,16 @@
+# Adding integers
+
+
+# Welcome User!
+print("Welcome to the Addittion Programme.")
+# Declaring variables
+a = input("Enter first number: ")
+b = input("Enter second number:")
+
+# Converting strings to intergers
+a = int(a)
+b = int(b)
+
+# Print a concatenation of results. Note: seperate each component with a comma.
+print("The results for", str(a), "+", str(b), "=", a+b)
+# print("The results for:", str(a), "+", str(b),"=",a+b)
